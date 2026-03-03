c2ee21cf00ae2be43519140e01ebf076a0b3d766
Dustin Delatore <dustin@delatore.me>
2026-03-02 22:23:49 -0500

    add lsp config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -12,14 +12,15 @@ vim.o.shiftwidth = 2
 vim.g.mapleader = " "
 
 -- Keymaps
-vim.keymap.set({"n", "v", "x"}, "<leader>y", '"*y')
-vim.keymap.set({"n", "v", "x"}, "<leader>d", '"*d')
-vim.keymap.set({"n"}, "<leader>f", ":Pick files<CR>")
--- vim.keymap.set({"n"}, "<leader>lf", vim.lsp.buf.format)
+vim.keymap.set({ "n", "v", "x" }, "<leader>y", '"*y')
+vim.keymap.set({ "n", "v", "x" }, "<leader>d", '"*d')
+vim.keymap.set({ "n" }, "<leader>f", ":Pick files<CR>")
+vim.keymap.set({ "n" }, "<leader>lf", vim.lsp.buf.format)
 
 --Plugins
 vim.pack.add({
 	{ src = "git@github.com:catppuccin/nvim.git" },
+	{ src = "git@github.com:neovim/nvim-lspconfig.git" },
 	{ src = "git@github.com:nvim-lualine/lualine.nvim.git" },
 	{ src = "git@github.com:nvim-mini/mini.pairs.git" },
 	{ src = "git@github.com:nvim-mini/mini.icons.git" },
@@ -29,23 +30,50 @@ vim.pack.add({
 
 vim.cmd("colorscheme catppuccin-mocha")
 
-require "gitsigns".setup()
+vim.lsp.enable {
+	"lua_ls"
+}
+
 require "mini.pairs".setup()
 require "mini.icons".setup()
 
+require "gitsigns".setup {
+	signs = {
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "" },
+		topdelete = { text = "" },
+		changedelete = { text = "▎" },
+		untracked = { text = "▎" },
+	},
+	signs_staged = {
+		add = { text = "▎" },
+		change = { text = "▎" },
+		delete = { text = "" },
+		topdelete = { text = "" },
+		changedelete = { text = "▎" },
+	},
+}
+
 -- Centered on screen
 local pick_window = function()
-  local height = math.floor(0.618 * vim.o.lines)
-  local width = math.floor(0.618 * vim.o.columns)
-  return {
-    anchor = 'NW', height = height, width = width,
-    row = math.floor(0.5 * (vim.o.lines - height)),
-    col = math.floor(0.5 * (vim.o.columns - width)),
-  }
+	local height = math.floor(0.618 * vim.o.lines)
+	local width = math.floor(0.618 * vim.o.columns)
+	return {
+		anchor = 'NW',
+		height = height,
+		width = width,
+		row = math.floor(0.5 * (vim.o.lines - height)),
+		col = math.floor(0.5 * (vim.o.columns - width)),
+	}
 end
 
 require "mini.pick".setup {
-	window = { config = pick_window }
+	window = {
+		config = pick_window,
+		prompt_prefix = "  ",
+		prompt_caret = "󰗧"
+	}
 }
 
 local filetype_icon = function()
@@ -58,16 +86,15 @@ require "lualine".setup {
 		section_separators = { left = '', right = '' },
 	},
 	sections = {
-		lualine_a = {{"mode", separator = { left = '' }, right_padding = 2 }  },
-		lualine_b = {"branch"},
+		lualine_a = { { "mode", separator = { left = '' }, right_padding = 2 } },
+		lualine_b = { "branch" },
 		lualine_c = {
 			{ filetype_icon, separator = "", padding = { left = 1, right = 0 } },
-			{"filename"}
+			{ "filename" }
 		},
-		lualine_x = {"diff"},
+		lualine_x = { "diff" },
 		lualine_z = {
-      { 'location', separator = { right = '' }, left_padding = 2 },
-    },
+			{ 'location', separator = { right = '' }, left_padding = 2 },
+		},
 	}
 }
-
